808546bac7c19b1d1f8a28c7546f4812d1fb5182
Ayuth Mangmesap <blackSource@windowslive.com>
2017-06-06 21:45:23 +0700

    Initial project

---
+++ GarenaLoader/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ini;
+
+namespace GarenaLoader
+{
+    class Garena
+    {
+        private readonly String SETTING_FILENAME = "GarenaSetting.txt";
+        public String Path
+        {
+            get;
+            set;
+        }
+
+        public void StartProgram()
+        {
+
+            String CurrentDirectory = Directory.GetCurrentDirectory();
+
+            //find settings file and load
+            try
+            {
+
+            }
+            catch (FileNotFoundException ex)
+            {
+                
+            }
+
+        }
+    }
+    
+    class Program
+    {
+        public void StartUp()
+        {
+
+
+            Garena garena = new Garena();
+            garena.Path = "eiei";
+            Console.WriteLine(garena.Path);
+            Console.Read();
+        }
+        static void Main(string[] args)
+        {
+            new Program().StartUp();
+
+
+
+
+
+
+
+        }
+    }
+}
