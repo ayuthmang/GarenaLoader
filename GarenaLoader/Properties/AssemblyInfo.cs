198c163db0511ec897c41d82876797f9703def07
Ayuth Mangmesap <blackSource@windowslive.com>
2017-06-09 03:37:04 +0700

    Add some information to file

--- GarenaLoader/Properties/AssemblyInfo.cs
+++ GarenaLoader/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GarenaLoader")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Created by blackSource")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("iC4RE")]
 [assembly: AssemblyProduct("GarenaLoader")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
